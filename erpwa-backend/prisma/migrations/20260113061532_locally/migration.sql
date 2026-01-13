e0985e354de2c4b81e9b27c9f59c8b1753bb92cb
developers-sitarahub <developers@sitarahub.com>
2026-01-13 12:20:16 +0530

    feat: add initial migration files for database schema including Campaign, User, and related entities

--- erpwa-backend/prisma/migrations/20260113061532_locally/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isOnline" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "lastLoginAt" TIMESTAMP(3),
-ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'active';
